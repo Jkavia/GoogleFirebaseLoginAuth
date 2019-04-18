ee9c094926ad5207075446c65549e28197dba2fc
Jkavia <Jkavia@github.com>
2019-04-18 11:14:15 -0400

    commit

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,22 @@
+
+
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+    if (user) {
+        //get data
+
+db.collection('guides').get().then(snapshot => {
+    console.log(snapshot.docs);
+    setupGuides(snapshot.docs);
+  });
+      //window.alert('user logged in: ', user);
+    } else {
+        setupGuides([]);
+      window.alert('user logged out');
+    }
+  });
+
+
 // signup
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
@@ -33,8 +52,49 @@ signupForm.addEventListener('submit', (e) => {
 const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) => {
   e.preventDefault();
-  auth.signOut().then(() => {
-      window.alert('user signed out');
-    console.log('user signed out');
-  })
+  auth.signOut().catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // [START_EXCLUDE]
+    if (errorCode == 'auth/weak-password') {
+      alert('The password is too weak.');
+    } else {
+      alert(errorMessage);
+    }
+    console.log(error);
+    // [END_EXCLUDE]
+  });
+});
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  // log the user in
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    console.log(cred.user);
+    // close the signup modal & reset form
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // [START_EXCLUDE]
+    if (errorCode == 'auth/weak-password') {
+      alert('The password is too weak.');
+    } else {
+      alert(errorMessage);
+    }
+    console.log(error);
+    // [END_EXCLUDE]
+  });
+
 });
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,26 @@
+// DOM elements
+const guideList = document.querySelector('.guides');
+
+// setup guides
+const setupGuides = (data) => {
+
+        if(data.length){
+    let html = '';
+    data.forEach(doc => {
+        const guide = doc.data();
+        console.log(doc.data);
+        const li = `<li>
+            <div class="collapsible-header grey lighten-1"> ${guide.title} </div>
+            <div class="collapsible-body white"> ${guide.content} </div>
+        </li>`;
+        html += li;
+    });
+    guideList.innerHTML = html
+    }else{
+        guideList.innerHTML = '<h5 class="center-align">Login to view Profile</h5>';
+    }
+};
+
 // setup materialize components
 document.addEventListener('DOMContentLoaded', function() {
 
